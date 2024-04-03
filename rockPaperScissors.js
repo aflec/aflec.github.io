a0e47e14e1396b26e89a49aacea1e47e90360a9f
André <andflec@gmail.com>
2024-04-02 22:29:39 -0300

    versao antes de implementar o jogo com varias rodadas e apresentar o vencedor final

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,15 +1,14 @@
 // JS part of Rock Paper Scissors
+//https://chat.openai.com/share/9e1ea0c9-729f-4539-adf5-59c9c09c395f -> esse é um link com uma forma de fazer um input por parte do usuário
 
 const opcoesParaJogar = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
-    // buscar como usar uma selecao aleatoria (tipo random)
-    //retornar 'Rock', 'Paper', 'Scissors';
     let indiceAleatorio = Math.floor(Math.random() * opcoesParaJogar.length);
     return opcoesParaJogar[indiceAleatorio];
 }
 
-function playSingleRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     console.log(`A opcao do usuario jogador foi: ${playerSelection}`);
     console.log(`A opcao do computador foi: ${computerSelection}`);
     if (playerSelection === 'rock' && computerSelection === 'rock') {
@@ -28,9 +27,35 @@ function playSingleRound(playerSelection, computerSelection) {
 	return "You lose! Rock beats Scissors"; 
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
 	return "You Win! Scissors beats Paper"; 
-    } else {
-	return "testando";
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+	return "Tie";
     }    
 }
 
-console.log(`Resultado da rodada: ${playSingleRound(process.argv[2], getComputerChoice())}`);
+console.log(`Resultado da rodada: ${playRound(process.argv[2], getComputerChoice())}`);
+
+function playGame(numeroDeVezesAJogar) {
+    let scoreUsuario = 0;
+    let socreComputador = 0;
+    //    for(i = 1; i <= numeroDeVezesAJogar; i++) {
+//	if (result === "You Win! Rock beats Scissors") {
+	scoreUsuario = scoreUsuario + 1;
+//	}
+//    }
+    return scoreUsuario;
+}
+
+console.log(playGame(5));
+/*
+function playGame(numeroDeVezesAJogar) {
+    for(i = 1; i <= numeroDeVezesAJogar; i++) { 
+	let scoreUsuario = 0;
+	let scoreComputador = 0;
+	if (playRound === 'kjdhkjdhs') {
+	    
+        }
+   }
+}
+
+playGame(5);
+*/
