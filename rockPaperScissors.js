548c7cea2887f98cb01c618e95f754cfe036d4c4
André <andflec@gmail.com>
2024-04-24 16:34:44 -0300

    ultimo commit antes de criar a branch rps-ui

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,6 +1,21 @@
 // JS part of Rock Paper Scissors
 //https://chat.openai.com/share/9e1ea0c9-729f-4539-adf5-59c9c09c395f -> esse é um link com uma forma de fazer um input por parte do usuário
 
+/*
+  let entrada = prompt('este eh um teste para o html');
+//console.log(entrada);
+
+// Capturando o texto usando prompt
+//var texto = prompt("Digite algo:");
+
+// Selecionando o elemento HTML pelo ID
+var container = document.getElementById('container');
+
+// Atualizando o conteúdo do elemento HTML
+container.innerHTML = "<p>O texto capturado é: " + entrada + "</p>";
+*/
+
+const readline = require('readline');
 const opcoesParaJogar = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
@@ -9,8 +24,13 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    if (!['rock', 'paper', 'scissors'].includes(playerSelection)) {
+	//	throw new Error("Valor inválido! Digite sua opcão novamente");
+	console.error("Valor inválido! Digite sua opcão novamente");
+	return;
+    }
     console.log(`A opcao do usuario jogador foi: ${playerSelection}`);
-    console.log(`A opcao do computador foi: ${computerSelection}`);
+    console.log(`A opcao do computador foi: ${computerSelection}`); 
     if (playerSelection === 'rock' && computerSelection === 'rock') {
 	return "Tie";
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
@@ -27,64 +47,60 @@ function playRound(playerSelection, computerSelection) {
 	return "You Lose! Rock beats Scissors"; 
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
 	return "You Win! Scissors beats Paper"; 
-    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-	return "Tie";
     } else {
-	return "Valor inválido! Digite sua opcão novamente"
+	return "Tie";
     }
 }
 //console.log(`Resultado da rodada: ${playRound(process.argv[2], getComputerChoice())}`);
 //console.log("aqui é o final da funcao playRound");
 
-function playGame(numeroDeVezesAJogar) {
-    let rodada = 0;
-    let contagemDoUsuario = 0;
-    let contagemDoComputador = 0;
-    let contagemDeEmpate = 0;
-    //OBS: essas variaveis de contagem tem que ficar de fora do FOR pq senao tada vez que
-    //voltar o loop elas vao receber o valor 0 novamente.
-    let resultadosFinais = {scoreUsuario: 0,
-			    scoreComputador: 0,
-			    scoreEmpate: 0
-			   };
-    //resultadoFinais tem que ficar de fora do FOR pra nao zerar os valores e
-    //para ficar no escopo certo para eu usar o "return resultadosFinais" fora do for
-    
-    for(i = 1; i <= numeroDeVezesAJogar; i++) {
-//	let resultadosFinais = {scoreUsuario: 0,
-//				scoreComputador: 0,
-//				scoreEmpate: 0
-//			       };
-	let result = playRound(process.argv[2], getComputerChoice());
-//	console.log("este é o log do result: " + result);
-//	let resultadoComecaComYouWin = result.startsWith("You Win");
-//	console.log("este é o log do resultadoComecaComYouWin: " + resultadoComecaComYouWin);
-//	let resultadoComecaComYouLose = result.startsWith("You Lose");
-//	console.log("este é o log do resultadoComecaComYouLose: " + resultadoComecaComYouLose);
-//	let resultadoIgualTie = result.startsWith("Tie");
-//	console.log("este é o log do resultadoIgualTie: " + resultadoIgualTie);
+function withUserChoice(rl, banana) {
+    rl.question('Digite algo: ', banana);
+}
 
-	if (result.startsWith("You Win")) { //a funcao starts.With ja retorna um valor booleano
-	    resultadosFinais.scoreUsuario++;
-	} else if (result.startsWith("You Lose")) {
-	    resultadosFinais.scoreComputador++;
-	} else {
-	    resultadosFinais.scoreEmpate++;
-	}
-//	console.log(resultadosFinais);
-	rodada = rodada + 1;
-    }
-    return resultadosFinais;
-
-    if (resultadosFinais.scoreUsuario > resultadosFinais.scoreComputador) {
-	console.log("O USUARIO venceu!");
-    } else if (resultadosFinais.scoreUsuario < resultadosFinais.scoreComputador) {
-	console.log("O COMPUTADOR venceu!");
-    } else {
-	console.log("EMPATE");
+function evaluateRound(userChoice, resultadosFinais) {
+    let result = playRound(userChoice, getComputerChoice());
+    if (result.startsWith("You Win")) { //a funcao starts.With ja retorna um valor booleano
+	resultadosFinais.scoreUsuario++;
+    } else if (result.startsWith("You Lose")) {
+	resultadosFinais.scoreComputador++;
     }
 }
 
-console.log(playGame(3));
+function playGame(numeroDeVezesAJogar, rl, resultadosFinais, rodada = 0) {
+  
+    
+    withUserChoice(rl, userChoice => {
+	evaluateRound(userChoice, resultadosFinais);
+	if (rodada <= numeroDeVezesAJogar) {
+	    rodada++;
+	    playGame(numeroDeVezesAJogar, rl, resultadosFinais, rodada);
+	}
+    }
+		  );
+    if (rodada == numeroDeVezesAJogar) {
+	rl.close();
+	if (resultadosFinais.scoreUsuario > resultadosFinais.scoreComputador) {
+	    console.log("O USUARIO venceu!");
+	} else if (resultadosFinais.scoreUsuario < resultadosFinais.scoreComputador) {
+	    console.log("O COMPUTADOR venceu!");
+	} else {
+	    console.log("EMPATE");
+	}
+	const scoreEmpate = numeroDeVezesAJogar - resultadosFinais.scoreUsuario - resultadosFinais.scoreComputador;
+	console.log({...resultadosFinais, scoreEmpate});
+    }
+}
 
 
+	//só quer executar a linha de baixo se resultadosFinais NAO tiver preenchido
+const resultadosFinais = {scoreUsuario: 0,
+			    scoreComputador: 0,
+		       };
+  
+  const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+    });
+
+playGame(3, rl, resultadosFinais);
