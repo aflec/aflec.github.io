90c52d0a5dcfa519c5daeb3ed19ad60c7a45e88d
André <andflec@gmail.com>
2024-05-03 06:44:25 -0300

    novas funcionalidades como: botoes habilitados/desabilitados de acordo com o andamento do jogo, computerChoice sendo mostrado como '?????' quando nao é pra aparecer

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -8,14 +8,64 @@ var container = document.getElementById('container');
 container.innerHTML = "<p>O texto capturado é: " + entrada + "</p>";
 */
 
+//desabilitar o botao jogar depois que os botoes de escolha forem habilitados e habilitar quando uma opcao do usuario for escolhida
+//limpar a opcao do computador quando o usuario escolher outra opcao
+//colocar as moedinhas como vidas para as rodadas do usuario
+
+function habilitarBotoes() {
+    const select = document.querySelector("#escolha");
+    const opcaoSelecionada = select.options[select.selectedIndex].value;
+    if (opcaoSelecionada === "0") { // Quando o valor selecionado for "0" ou "-"
+        document.querySelector("#btnRock").disabled = true;
+        document.getElementById("btnPaper").disabled = true;
+        document.getElementById("btnScissors").disabled = true;
+	document.querySelector("#btnPlayGame").disabled = true;
+    } else {
+        document.querySelector("#btnRock").disabled = false;
+        document.getElementById("btnPaper").disabled = false;
+        document.getElementById("btnScissors").disabled = false;
+//	document.querySelector("#btnPlayGame").disabled = false;
+    }
+}
+
+habilitarBotoes();
+
 function getComputerChoice() {
     const opcoesParaJogar = ['rock', 'paper', 'scissors'];
     let indiceAleatorio = Math.floor(Math.random() * opcoesParaJogar.length);
     return opcoesParaJogar[indiceAleatorio];
 }
 
-function pickUserChoice(userChoice) {
+let fichas = 0;
 
+function mostrarOpcaoSelecionada() {
+
+    const select = document.querySelector("#escolha");
+    const opcaoSelecionada = select.options[select.selectedIndex].value;
+    if(opcaoSelecionada === "-") {
+	document.querySelector("#btnRock").disabled = true;
+	document.getElementById("btnPaper").disabled = true;
+	document.getElementById("btnScissors").disabled = true;
+    }
+    console.log(opcaoSelecionada);
+    fichas = opcaoSelecionada;
+}
+mostrarOpcaoSelecionada();
+
+let userChoice;
+
+function pickUserChoice(escolhaUsuario) {
+    const target = document.querySelector("#target");
+    target.textContent = escolhaUsuario;
+    userChoice = escolhaUsuario;
+    document.querySelector("#btnPlayGame").disabled = false;
+
+    const computador = document.querySelector("#computador");
+    computador.textContent = "?????";
+}
+
+
+function playGame() {
     const computerChoice = getComputerChoice();
     const resultado = playRound(userChoice, computerChoice);
 
@@ -26,7 +76,7 @@ function pickUserChoice(userChoice) {
     const resultJogada = document.querySelector("#container");
     resultJogada.textContent = resultado;
 }
-
+ 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'rock') {
 	return "Tie";
@@ -48,3 +98,5 @@ function playRound(playerSelection, computerSelection) {
 	return "Tie";
     }
 }
+
+  
